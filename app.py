9584d746b6066e346c3e1d084ea8e021e6f46d8f
bottych8ck <143306211+bottych8ck@users.noreply.github.com>
2023-12-17 20:49:35 +0100

    Update app.py

--- app.py
+++ app.py
@@ -42,7 +42,8 @@ def welcome_page():
     - Die Applikation basiert auf der sog. RAG-Technik (Retrieval Augmented Generation). Dabei werden einem LLM bei einer Anfrage passende Informationen vorgelegt, die für die Beantwortung genutzt werden können.
     - Aus Kostengründen erfolgt keine direkte Beantwortung der Frage in der Applikation, weshalb die User den Prompt lediglich kopieren und ihn danach selbst einem LLM vorlegen können.   
     - Der Datenschutz kann gegenwärtig nicht garantiert werden. Verwenden Sie daher keine Personendaten in Ihrer Anfrage.
-    - Die Applikation liefert eine Übersicht der semantisch und kontextuell am besten auf die Anfrage passenden Artikel und generiert daraus einen Prompt. Weder die tatsächliche Anwendbarkeit der ausgewählten Artikel noch die Richtigkeiten der Antwort des LLM kann garantiert werden.    
+    - Die Applikation liefert eine Übersicht der semantisch und kontextuell am besten auf die Anfrage passenden Bestimmungen und generiert daraus einen Prompt. Weder die tatsächliche Anwendbarkeit der ausgewählten Bestimmungen noch die Richtigkeiten der Antwort des LLM kann garantiert werden.    
+    - Selbst bei Fragen, die nicht direkt das Gemeinderecht des Kantons Thurgau betreffen, sucht das System nach den am besten übereinstimmenden Bestimmungen innerhalb dieses Rechtsbereichs. Beachten Sie jedoch, dass in solchen Fällen die ausgewählten Bestimmungen möglicherweise nicht zutreffend oder relevant sind.
     """)
    
 
@@ -168,7 +169,7 @@ def main_app():
             similarities = calculate_similarities(query_vector, {title: article_embeddings[title] for title in relevant_lawcontent_dict if title in article_embeddings})
             sorted_articles = sorted(similarities.items(), key=lambda x: x[1], reverse=True)[:5]  # Get only top 5 articles
             st.session_state.top_articles = sorted_articles  # Update session state
-            st.write("Die folgenden Artikel werden angezeigt, nachdem Ihre Anfrage analysiert und mit den relevanten Gesetzesdaten abgeglichen wurde. Dieser Prozess funktioniert ähnlich wie eine intelligente Suche, bei der die Bedeutung Ihrer Worte erkannt und die passendsten Inhalte aus den Gesetzestexten ausgewählt werden. Ihre Anwendbarkeit hin überprüft werden. Diese Überprüfung kann durch ein LLM (Large Language Model) erfolgen, das die Anwendbarkeit prüft und eine Antwort auf die Anfrage formuliert.")
+            st.write("Die folgenden Artikel werden angezeigt, nachdem Ihre Anfrage analysiert und mit den relevanten Gesetzesdaten abgeglichen wurde. Dieser Prozess funktioniert ähnlich wie eine intelligente Suche, bei der die Bedeutung Ihrer Worte erkannt und die passendsten Inhalte aus den Gesetzestexten ausgewählt werden. Die Bestimmungen müssen aber genau auf ihre tatächliche Anwendbarkeit hin überprüft werden. Diese Überprüfung kann durch ein LLM (Large Language Model) unterstützt werden. Im generierten Prompt sind entsprechende Anweisungen enthalten.")
 
             with st.expander("Am besten auf die Anfrage passende Artikel", expanded=False):
                 for title, score in st.session_state.top_articles:
@@ -185,11 +186,11 @@ def main_app():
             st.warning("Bitte geben Sie eine Anfrage ein.")
             
     if st.session_state.submitted:
-        if st.button("Generate Prompt"):
+        if st.button("Prompt generieren"):
             if user_query and st.session_state.top_articles:
                 # Generate and display the prompt
                 prompt = generate_prompt(user_query, relevance, st.session_state.top_articles, law_data)
-                st.text_area("Generated Prompt:", prompt, height=300)
+                st.text_area("Prompt:", prompt, height=300)
                 
                 # Button to copy the prompt to clipboard
                 html_with_js = generate_html_with_js(prompt)
