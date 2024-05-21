1485ec5bafe2410c65866ba368967e00de28c061
bottych8ck <143306211+bottych8ck@users.noreply.github.com>
2024-05-21 16:49:08 +0200

    Update app.py

--- app.py
+++ app.py
@@ -175,7 +175,7 @@ def main_app():
             query_vector = get_embeddings(user_query)
             prompt = generate_prompt(user_query, relevance, st.session_state.top_articles, law_data, st.session_state.top_knowledge_items)
             response = client.chat.completions.create(
-                model="gpt-4-turbo",
+                model="gpt-4o",
                 messages=[
                     {"role": "system", "content": "Du bist eine Gesetzessumptionsmaschiene. Du beantwortest alle Fragen auf Deutsch."},
                     {"role": "user", "content": prompt}
