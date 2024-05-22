a31a23ba714ac285d416b7a5124f3d129fa26287
bottych8ck <143306211+bottych8ck@users.noreply.github.com>
2024-05-22 07:59:05 +0200

    Update app.py

--- app.py
+++ app.py
@@ -139,7 +139,7 @@ def generate_prompt(user_query, relevance, top_articles, law_data, top_knowledge
 
 
 def main_app():
-    st.title("Subsumary - Chat_TG")
+    st.title("Subsumary")
     st.subheader("Abfrage des Thurgauer Personalrechts")
     if 'last_question' not in st.session_state:
         st.session_state['last_question'] = ""
