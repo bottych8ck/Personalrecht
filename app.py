a75a2b2dc1e4c030613538afde26bb6377d33bdd
bottych8ck <143306211+bottych8ck@users.noreply.github.com>
2024-06-02 14:16:25 +0200

    Update app.py

--- app.py
+++ app.py
@@ -152,6 +152,7 @@ def generate_prompt(user_query, relevance, top_articles, law_data, top_knowledge
    
     prompt += "Anfrage auf Deutsch beantworten. Prüfe die  Anwendbarkeit der einzelnen § genau. Wenn ein Artikel keine einschlägigen Aussagen enthält, vergiss ihn.\n"
     prompt += "Mache nach der Antwort ein Fazit und erwähne dort die relevanten § mitsamt dem Erlassnahmen \n"
+    prompt += "Anfrage auf Deutsch beantworten \n"
 
     return prompt
 
